1daf4539130d3668c299822d062b43e75f0fbd88
AJT <lunegeoid@gmail.com>
2024-02-08 12:41:36 -0600

    Update home.jsx
    adding conditional messages for loading blog data

--- src/home.jsx
+++ src/home.jsx
@@ -3,18 +3,25 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs]= useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
+       setTimeout(() =>{
         fetch('http://localhost:3000/blogs')
-            .then(res =>{
-                return res.json();
-            })
-            .then((data)=>{
-                setBlogs(data);
-            })
+        .then(res =>{
+            return res.json();
+        })
+        .then((data)=>{
+            setBlogs(data);
+            setIsPending(false);
+        });
+
+       },1000); 
     },[]);
     return ( 
     <div className="home">
+    
+        {isPending && <div>Loading...</div>}
         {blogs && <BlogList blogs={blogs} title="All Blogs" />}
 
     </div> 
