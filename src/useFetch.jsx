9a2e015f9644dfbd3cedab32dca93dcbbcf7c483
AJT <lunegeoid@gmail.com>
2024-02-22 17:59:05 -0600

    used cleanup to abort fetch
    used cleanup to abort fetch, optimized imports in home

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -1,34 +1,38 @@
 import { useState, useEffect } from "react";
 
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-const useFetch = (url)=> {
+  useEffect(() => {
+    const abortCont = new AbortController();
 
-const [data, setData]= useState(null);
-const [isPending, setIsPending] = useState(true);
-const [error, setError]= useState(null);
-
-useEffect(() => {
-   setTimeout(() =>{
-    fetch(url)
-        .then(res =>{
-            if(!res.ok){
-                throw Error('could not fetch data')
-            }
-            return res.json();
+    setTimeout(() => {
+      fetch(url, { signal: abortCont.signal })
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("could not fetch data");
+          }
+          return res.json();
         })
-        .then((data)=>{
-            setData(data);
-            setIsPending(false);
-            setError(null);
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
         })
-        .catch(err =>{
+        .catch((err) => {
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
             setIsPending(false);
             setError(err.message);
-        })
-
-   },1000); 
-},[url]);
-return {data, isPending, error}
-}
+          }
+        });
+    }, 1000);
+    return () => abortCont.abort();
+  }, [url]);
+  return { data, isPending, error };
+};
 
 export default useFetch;
